c09472e3c60c4710b8ffdc467146ed81dd77ee75
Kiwicase <emailcaseydent@gmail.com>
2022-06-29 16:47:24 +0300

    Working randome number generator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Random rnd = new Random();
+            int rNumber = rnd.Next(0, 101);
+            Console.WriteLine(rNumber);
         }
     }
 }
